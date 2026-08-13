5932f6a9a455c4c92d6dfd6d0b1c96b3f6cbf9e5
GRUPOCLER <gestion@grupocler.com.mx>
2026-08-13 17:49:20 -0600

    commit and push

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  server: { port: 5173 },
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: true
+  }
 })
