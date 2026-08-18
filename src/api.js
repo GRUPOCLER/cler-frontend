a2df8404245ecc5ae58dd93d2d47ee86680a3a91
GRUPOCLER <gestion@grupocler.com.mx>
2026-08-18 14:38:36 -0600

    Add update delivery function to API client
    Added function to update deliveries in the API client.

--- src/api.js
+++ src/api.js
@@ -44,6 +44,7 @@ export async function login(usuario, password) {
 export const listarEntregas   = (params = '') => req('/api/entregas/' + (params ? '?' + params : ''))
 export const detalleEntrega   = (id)          => req('/api/entregas/' + id)
 export const crearEntrega     = (body)        => req('/api/entregas/', { method: 'POST', body: JSON.stringify(body) })
+export const actualizarEntrega= (id, body)    => req('/api/entregas/' + id, { method: 'PATCH', body: JSON.stringify(body) })
 export const completarEntrega = (id)          => req('/api/entregas/' + id + '/completar', { method: 'POST' })
 export const getDashboard     = ()            => req('/api/dashboard/')
 
